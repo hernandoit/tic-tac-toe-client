783ee79cbc8cf02331f36d3a577228fba3b53916
hernandoit <hernandohernandezit@gmail.com>
2021-07-20 13:02:33 -0400

    update script from a GET to a PATCH method

--- curl-scripts/game/update.sh
+++ curl-scripts/game/update.sh
@@ -8,7 +8,7 @@
 
 curl "https://tic-tac-toe-api-development.herokuapp.com/games/${ID}" \
   --include \
-  --request GET \
+  --request PATCH \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
